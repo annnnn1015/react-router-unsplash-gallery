d5c52e87db515daf279477a14d75682354697f5c
Anna <s12345abcd5@gmail.com>
2025-07-07 16:28:45 +0800

    feat: 新增 Home 和 AlbumIndex 頁面。 建立Album路由，以及子路由AlbumIndex 且設為預設首頁index。 fix: App 元件- 1. 加入Navbar元件，使導覽列置頂每個頁面 2. 修改首頁路由為Home頁面

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
+import Album from "./pages/Album";
+import AlbumIndex from "./pages/AlbumIndex";
+import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="Container">
-      <Routes>
-        <Route path="/" element={<Navbar />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
+    <div className="App">
+      <Navbar />
+      <div className="Container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/album" element={<Album />}>
+            <Route index element={<AlbumIndex />}></Route>
+          </Route>
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/pages/AlbumIndex.js
@@ -0,0 +1,7 @@
+export default function AlbumIndex() {
+  return (
+    <div>
+      <h1>AlbumIndex</h1>
+    </div>
+  );
+}
