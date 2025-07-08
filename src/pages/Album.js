31dea0caebc2ff1ab2a0d972980fc204bba9c432
Anna <s12345abcd5@gmail.com>
2025-07-08 10:38:36 +0800

    feat: 在 Album 頁面中新增 Unsplash API 的請求設定

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -2,6 +2,11 @@ import { Outlet } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
+const api = "https://api.unsplash.com/search/photos";
+const accessId = process.env.REACT_APP_UNSPLASH_ACCESS;
+
+console.log(api, accessId);
+
 export default function Album() {
   const [list, setList] = useState([]);
 
