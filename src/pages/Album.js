f17c34b85df98afba70b37748f63186df6f980c8
Anna <s12345abcd5@gmail.com>
2025-07-08 11:19:05 +0800

    feat: 在 Album 頁面中新增 useEffect 及從unsplash請求圖片資源

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const api = "https://api.unsplash.com/search/photos";
 const accessId = process.env.REACT_APP_UNSPLASH_ACCESS;
@@ -9,6 +9,16 @@ console.log(api, accessId);
 
 export default function Album() {
   const [list, setList] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(
+        `${api}?client_id=${accessId}&query=animal`
+      );
+      // query=animal 意思是搜尋關鍵字為animal
+      console.log(response);
+    })();
+  }, []);
 
   return (
     <div className="row">
