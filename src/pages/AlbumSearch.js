32de724e1432d115021faa714f934a1c2f846892
Anna <s12345abcd5@gmail.com>
2025-07-15 16:59:32 +0800

    實作搜尋圖片功能：4. 搜尋頁面- 使用onKeyUp判斷按下Enter時儲存輸入框的值

--- src/pages/AlbumSearch.js
+++ src/pages/AlbumSearch.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export default function AlbumSearch() {
+  const [search, setSearch] = useState("");
+
   return (
     <div>
-      <h3>圖片搜尋</h3>
-      <input type="text" />
+      <h3>請輸入關鍵字</h3>
+      <input
+        type="text"
+        className="form-control"
+        defaultValue={search}
+        onKeyUp={(e) => {
+          if (e.code === "Enter") {
+            setSearch(e.target.value);
+          }
+        }}
+      />
     </div>
   );
 }
