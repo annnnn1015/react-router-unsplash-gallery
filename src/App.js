e6a3b4f36b6cb73917673c32cd1e3799f6279597
Anna <s12345abcd5@gmail.com>
2025-07-15 14:14:05 +0800

    實作動態路由: 2. 在路油表設定動態路由

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Album from "./pages/Album";
 import AlbumIndex from "./pages/AlbumIndex";
+import AlbumPage from "./pages/AlbumPage";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/about" element={<About />}></Route>
           <Route path="/album" element={<Album />}>
             <Route index element={<AlbumIndex />}></Route>
+            <Route path="/album/:id" element={AlbumPage}></Route>
           </Route>
         </Routes>
       </div>
