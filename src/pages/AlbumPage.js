c19e1684f935660f1f1c0a1cb273a8287746f959
Anna <s12345abcd5@gmail.com>
2025-07-21 18:33:30 +0800

    自訂上一頁按鈕：用useNavigate搭配按鈕onClick，navigate(-1) 是上一頁，navigate(1)是後一頁

--- src/pages/AlbumPage.js
+++ src/pages/AlbumPage.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const api = process.env.REACT_APP_BASE_URL;
 const accessId = process.env.REACT_APP_UNSPLASH_ACCESS;
 
 export default function AlbumPage() {
+  const navigate = useNavigate();
+
   const { id } = useParams();
   // 從路由表path取出id的值
   // console.log(useParams());
@@ -25,6 +28,20 @@ export default function AlbumPage() {
   return (
     <>
       <div>
+        <button
+          type="button"
+          className="btn btn-primary me-3"
+          onClick={() => navigate(-1)}
+        >
+          -1 上一頁
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => navigate(1)}
+        >
+          1 下一頁（需先回到上一頁才能按）
+        </button>
         <ul className="list-unstyled">
           <li>圖片ID:{id}</li>
           <li>描述：{photoData.description}</li>
