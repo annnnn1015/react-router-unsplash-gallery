b0a5fe74898471782d0d32f686004614a3b61314
Anna <s12345abcd5@gmail.com>
2025-07-21 16:40:29 +0800

    NavLink 實作：1. 將Navbar中的Link元件替換為NavLink以支持活動狀態樣式

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -18,19 +18,19 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <NavLink className="nav-link" aria-current="page" to="/">
                 Home
-              </Link>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <NavLink className="nav-link" to="/about">
                 About
-              </Link>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/album">
+              <NavLink className="nav-link" to="/album">
                 Album
-              </Link>
+              </NavLink>
             </li>
             <li className="nav-item dropdown">
               <span
